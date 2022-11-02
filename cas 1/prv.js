31b845dccc22d6ba219bc57a7e550565dcae9835
Jana <janagligorova@yahoo.com>
2022-11-02 21:59:37 +0100

    remove empty spaces

---
+++ cas 1/prv.js
@@ -0,0 +1,5 @@
+console.log('Hello Semos!')
+
+console.warn('This is a warning.')
+
+console.error('FATAL ERROR')
---
+++ cas 1/variables.js
@@ -0,0 +1,38 @@
+// program 1
+
+// var - kluchen zbor za deklariranje/definiranje na globalni promenlivi
+// let - kluchen zbor za definiranje na lokalna promenliva
+// const - definiranje na konstanti
+
+let username = 'Dona', emailAddress = 'test@test.com'
+
+console.log(username, emailAddress)
+
+// so operatorot "=" zadavame vrednost za promenlivi
+username = 'Jana'
+
+// so console.log gi pechatime vrednostite na promenlivite
+console.log(username)
+
+const a = 5;
+const b = a;
+// vazhi za site operacii +,-,*,/, %(dava ostatok pri delenje)
+const zbir = a + b;
+console.log(zbir)
+
+// string concantenation (spojuvanje na tekst)
+console.log(username + zbir)
+
+const number = '10'
+// let test;
+// console.log(number + zbir + test)
+
+// ova e tekst, a ne broj
+const notANumber = '10'
+
+// null e prazna vrednost
+let test = neull;
+
+// undefined e nedefinirana vrednost
+test = undefined;
+console.log(test)
